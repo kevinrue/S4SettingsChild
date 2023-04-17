1f2dd1daadff6211031483c6de6ac6cb978895e9
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-04-17 15:05:45 +0100

    add documentation

--- R/settings.R
+++ R/settings.R
@@ -1,5 +1,10 @@
+#' Incremental Read Only Settings
+#'
+#' @format Named list of settings.
+#'
 #' @importFrom S4Settings IncrementalReadOnlySettings
 #' @export
+#' @docType data
 exampleSettings <- IncrementalReadOnlySettings(
   version = "1.0.0"
 )
